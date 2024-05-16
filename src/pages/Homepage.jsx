bbe7485ee61550e133d3d752b8aa230b5b655625
Xavier Rice <Xavier.Rice@gmail.com>
2024-05-15 20:26:39 -0400

    flip card running

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -47,7 +47,7 @@ const Homepage = () => {
         <ParallaxLayer offset={0} speed={0.5}>
         </ParallaxLayer>
         <ParallaxLayer offset={1} speed={0.5}>
-          <div><Cardflip/></div>
+          <Cardflip/>
         </ParallaxLayer>
         <ParallaxLayer offset={2} speed={0.5}>
           <div>Your content for page 3</div>
